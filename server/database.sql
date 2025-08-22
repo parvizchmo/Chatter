972ac201311ec46adfa1cc250ae6bc0a164f46a5
DenVlasParviz <59772685+parvizchmo@users.noreply.github.com>
2025-08-22 16:43:18 +0300

    Delete server directory

--- server/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE drivers (
-  id             SERIAL PRIMARY KEY,
-  driver_number  INTEGER    NOT NULL UNIQUE,
-  driver_name    VARCHAR(100) NOT NULL,
-  car_weight     INTEGER    NOT NULL,
-  weather_temp   INTEGER    NOT NULL,
-  weather_code   INTEGER    NOT NULL
-);
