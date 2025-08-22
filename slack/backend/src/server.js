9ec881047230203d0cd5136ee69ddfea1b2cb7f3
DenVlas1 <vlas2133@gmail.com>
2025-08-22 17:20:28 +0300

    db connected and env.js completed

--- slack/backend/src/server.js
+++ slack/backend/src/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import {ENV} from "../env.js"
+import {connectDB} from "../db.js";
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("index")
 })
 
-app.listen(ENV.PORT,()=>console.log('server is on',ENV.PORT))
+app.listen(ENV.PORT,()=>{
+    console.log('server is on',ENV.PORT)
+    connectDB()
+    }
+)
