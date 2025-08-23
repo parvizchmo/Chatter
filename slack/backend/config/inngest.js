c9d3b14eed8396480431300e0f4ac4c5c5999b08
DenVlas1 <vlas2133@gmail.com>
2025-08-23 16:02:39 +0300

    fixed inngest.js

--- slack/backend/config/inngest.js
+++ slack/backend/config/inngest.js
@@ -5,26 +5,28 @@ import {User} from "../models/user.model.js";
 export const inngest = new Inngest({id: "chatter"})
 
 const syncUser = inngest.createFunction(
-    {id: "sync-user"},
-    {event: "clerk/user.created"},
-    async ({event}) => {
-        await connectDB()
+    { id: "sync-user" },
+    { event: "Clerk/user.created" },
+    async ({ event }) => {
+        await connectDB();
+
         const { id, email_addresses, first_name, last_name, image_url } = event.data;
+
         const newUser = {
             clerkId: id,
             email: email_addresses[0]?.email_address,
             name: `${first_name || ""} ${last_name || ""}`,
-            image_url: image_url,
+            image: image_url,
+        };
 
-        }
-        await User.create(newUser)
+        await User.create(newUser);
         //T
     }
 )
 
 const deleteUserFromDB = inngest.createFunction(
     { id: "delete-user-from-db" },
-    { event: "clerk/user.deleted" },
+    { event: "Clerk/user.deleted" },
     async ({ event }) => {
         await connectDB();
         const { id } = event.data;
